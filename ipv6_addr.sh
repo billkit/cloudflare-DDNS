611c61a120dcb093e900875125bc55364d9485bd
junqi <39383468+junqi-chen@users.noreply.github.com>
2019-06-01 02:06:39 +0800

    Update ipv6_addr.sh

--- ipv6_addr.sh
+++ ipv6_addr.sh
@@ -5,9 +5,11 @@ Interface=$1
 
 if [ $# -eq 0 ];
 then
-   Interface=eth0
+   Interface=ens192
 else
    Interface=$1
 fi
       
-/sbin/ip -6 addr show dev $Interface| awk -F '[ \t]+|/' '$3 == "::1" { next;} $3 ~ /^fe80::/ { next ; } /inet6/ {print $3}'
+# /sbin/ip -6 addr show dev $Interface| awk -F '[ \t]+|/' '$3 == "::1" { next;} $3 ~ /^fe80::/ { next ; } /inet6/ {print $3}'
+# 显示24 开头的公网 ipv6 地址
+/sbin/ip -6 addr show dev $Interface| grep '24'|awk -F '[ \t]+|/' '$3 == "fdd2::" { next;} $3 ~ /^fe80:/ { next ; } /inet6/ {print $3}'
